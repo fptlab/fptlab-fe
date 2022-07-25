f18849ca70341eb2a85a41579a3d08e5abd96b46
AlessandroColonna <alessandro.colonna10@gmail.com>
2022-07-25 11:52:06 +0200

    - remove test

--- src/api/config.js
+++ src/api/config.js
@@ -1,4 +1,4 @@
-// localhost: http://localhost:8082 test
+// localhost: http://localhost:8082
 const API_BASE_DOMAIN = process.env.NODE_ENV === 'development' ? 'http://localhost:8082' : 'https://fptlab.herokuapp.com'
 export const API_BASE_URL = `${API_BASE_DOMAIN}/rest/api/`
 
