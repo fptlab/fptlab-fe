3e06c5724bb5994a6b6309cd367ffe52dfe7b549
AlessandroColonna <alessandro.colonna10@gmail.com>
2022-08-09 13:49:23 +0200

    - detail

--- src/pages/admin/handle-users/UserDetailPage.vue
+++ src/pages/admin/handle-users/UserDetailPage.vue
@@ -109,7 +109,7 @@ onMounted(async () => {
       </div>
     </div>
     <div class="q-pa-md">
-      <span class="text-h6 text-white">Le tue sottoscrizioni</span>
+      <span class="text-h6 text-white">Ultime sottoscrizioni</span>
       <div class="row no-wrap q-gutter-md overflow-auto q-py-md">
         <q-card :class="sub.active ? 'subscription-card subscription-card__active' : 'subscription-card subscription-card__expired'" v-for="sub in userSubscriptionList" :key="sub.id">
           <q-card-section class="row justify-between items-end">
@@ -129,6 +129,31 @@ onMounted(async () => {
       <div class="row justify-end items-center">
         <q-item tag="a" class="text-white items-center" :to="user.id + '/subscriptions'">Visualizza tutte</q-item>
         <q-btn text-color="white" label="Aggiungi" no-caps icon-right="more_time" class="bg-primary" @click="dialog = true; subscriptionSelected = false" push/>
+      </div>
+    </div>
+
+    <div class="q-pa-md">
+      <span class="text-h6 text-white">Ultime prenotazioni</span>
+      <div class="row no-wrap q-gutter-md overflow-auto q-py-md">
+        <q-card class="booking-card" v-for="sub in userSubscriptionList" :key="sub.id">
+          <em></em>
+          <div class="row bg-primary items-center justify-center text-white text-h6 booking-card__header text-bold">
+            10 AGOSTO
+          </div>
+          <q-card-section class="column justify-center items-center">
+            <div class="row justify-center items-center q-mb-sm">
+              <q-img src="../../../assets/workout_1h.png" v-if="sub.type === 'HOUR'" style="width: 70px"></q-img>
+              <q-img src="../../../assets/workout_half1h.png" v-if="sub.type === 'HALF_HOUR'" style="width: 70px"></q-img>
+            </div>
+            <div class="text-secondary text-bold text-h6">10:00 - 10:30</div>
+          </q-card-section>
+          <!--div class="row justify-between full-width" v-if="sub.active">
+            <q-btn text-color="white" label="Cancella" no-caps class="bg-red full-width" style="border-radius: 0px 0px 20px 20px"/>
+          </div-->
+        </q-card>
+      </div>
+      <div class="row justify-end">
+        <q-item tag="a" class="text-white no-padding" :to="user.id + '/subscriptions'">Visualizza tutte</q-item>
       </div>
     </div>
 
@@ -181,4 +206,52 @@ onMounted(async () => {
     border: 3px solid #638229;
   }
 }
+
+.booking-card {
+  min-width: 60vw;
+  border-radius: 20px;
+  box-shadow: 1px 1px 4px 0px #a69d9d7d;
+
+  &__header {
+    height: 50px;
+    border-radius: 20px 20px 0px 0px !important;
+  }
+}
+
+.booking-card:before, .booking-card:after{
+  content:'';
+  float:left;
+  position:absolute;
+  top:5px;
+  width:8px;
+  height:8px;
+  background:#111;
+  z-index:1;
+  -moz-border-radius:10px;
+  -webkit-border-radius:10px;
+  border-radius:10px;
+  -moz-box-shadow:0 1px 1px #fff;
+  -webkit-box-shadow:0 1px 1px #fff;
+  box-shadow:0 1px 1px #fff;
+}
+.booking-card:before{left:11px;}
+.booking-card:after{right:11px;}
+
+.booking-card em:before, .booking-card em:after{
+  content:'';
+  float:left;
+  position:absolute;
+  top:-5px;
+  width:4px;
+  height:14px;
+  background:#dadada;
+  background:-webkit-gradient(linear, left top, left bottom, from(#f1f1f1), to(#aaa));
+  background:-moz-linear-gradient(top,  #f1f1f1,  #aaa);
+  z-index:2;
+  -moz-border-radius:2px;
+  -webkit-border-radius:2px;
+  border-radius:2px;
+}
+.booking-card em:before{left:13px;}
+.booking-card em:after{right:13px;}
 </style>
